3060a8a57b43bab1fd0dafd6f1eb0552050283a7
Ahngbeom <bbu0704@gmail.com>
2022-08-10 14:32:51 +0900

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -6,9 +6,10 @@ printf "#vCPU : " && cat /proc/cpuinfo | grep processor | wc -l
 
 printf "#Memory Usage: " && free -m | grep Mem | awk '{printf "%d/%dMB (%.2f%%)\n", $3, $2, $3/$2 * 100}'
 
-printf "#Disk Usage: " && df -ah | grep /dev/mapper/bahn42--vg-root | awk '{printf "%d/%dGb (%d%%)\n", $4, $2, $5}'
+#printf "#Disk Usage: " && df -ah | grep /dev/mapper/bahn42--vg-root | awk '{printf "%d/%dGb (%d%%)\n", $4, $2, $5}'
+printf "#Disk Usage: " && df -ah | grep /dev/mapper/bahn42--vg-root | awk '{printf "%.1f/%.1fGb (%d%%)\n", $4, $2, $5}'
 
-printf "#CPU load: " && mpstat -P ALL | grep all | awk '{printf "%.2f%%\n", 100 - $13}'
+printf "#CPU load: " && mpstat | grep all | awk '{printf "%.2f%%\n", 100 - $13}'
 
 printf "#Last boot: " && who -b | awk '{printf $3" "$4"\n"}'
 
@@ -24,5 +25,5 @@ ss -alt | wc -l | awk '{printf "#Connections TCP : %d ESTABILISHED\n", $1 - 1}'
 
 
 printf "#User log: " && users | wc -w
-printf "#Network: IP " && echo -n $(hostname -I) && ip link | grep ether | sed '$d' | awk '{printf " ("$2")\n"}'
+printf "#Network: IP " && echo -n $(hostname -I) && ip link | grep ether | sed '2, $d' | awk '{printf " ("$2")\n"}'
 printf "#Sudo : " && echo -n $(journalctl _COMM=sudo | wc -l) && echo " cmd"
